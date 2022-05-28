e25c871f1368347d94865515669d860b06c83ff1
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-05-28 16:56:16 +0530

    added

---
+++ Generics 4/src/com/generics/Maximum.java
@@ -0,0 +1,56 @@
+package com.generics;
+
+public class Maximum <T extends Comparable<T>>{
+	T first;
+	T second;
+	T third;
+	T fourth;
+	T fifth;
+
+	public Maximum(T first, T second, T third, T fourth, T fifth) {
+		super();
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		this.fourth = fourth;
+		this.fifth = fifth;
+	}
+
+	public <T extends Comparable<T>> T findMax(T first, T second, T third, T fourth, T fifth) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0 && first.compareTo(fourth) >= 0 && first.compareTo(fifth) >= 0) {
+			System.out.println(" First is Largest ");
+			return first;
+		}
+
+		else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0 && second.compareTo(fourth) >= 0 && second.compareTo(fifth) >= 0) {
+			System.out.println(" Second is Largest");
+			return second;
+		}
+
+		else if (third.compareTo(first) >= 0 && third.compareTo(second) >= 0 && third.compareTo(fourth) >= 0 && third.compareTo(fifth) >= 0) {
+			System.out.println(" Third is Largest");
+			return third;
+		}
+
+		if (fourth.compareTo(first) >= 0 && fourth.compareTo(second) >= 0 && fourth.compareTo(third) >= 0 && fourth.compareTo(fifth) >= 0) {
+			System.out.println("Fourth is Largest");
+			return fourth;
+		}
+
+		else {
+			System.out.println("Fifth is Largest");
+			return fifth;
+		}
+	}
+
+	public void findMax() {
+		findMax(this.first, this.second, this.third, this.fourth, this.fifth);
+	}
+
+	public Maximum(T first, T second, T third) {
+		super();
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+}
